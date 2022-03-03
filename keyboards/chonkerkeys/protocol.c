05fa45a8c094c9d6dcb4d88171a90e760e303436
Rock <vzox.rock@gmail.com>
2022-03-03 17:43:00 +0800

    Fix not updating KEY_SIZE #chonkerkeys/214

--- keyboards/chonkerkeys/protocol.c
+++ keyboards/chonkerkeys/protocol.c
@@ -39,8 +39,8 @@ void _get_config_data_writer(void* user_data) {
 }
 
 #define LAYER_TYPE_SIZE 1
-// size ordinal = 1, key action type = 1, icon = 8, color = 4 * 2
-#define KEY_SIZE 18
+// size ordinal = 1, key action type = 1, icon = 8, color = 4 * 2, custom macros = 3
+#define KEY_SIZE 21
 
 void on_get_config() {
     const uint8_t layer_count = get_layer_count();
