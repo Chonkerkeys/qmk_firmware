cd2b84126c4fa31c122b672f92d893159d4b2192
Chanksy <49041488+Chanksy@users.noreply.github.com>
2022-08-03 15:48:46 +0800

    Disabled unused features TAPPING & ONESHOT

--- keyboards/chonkerkeys/max/config.h
+++ keyboards/chonkerkeys/max/config.h
@@ -110,8 +110,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* disable action features */
 //#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+#define NO_ACTION_TAPPING
+#define NO_ACTION_ONESHOT
 
 /* disable these deprecated features by default */
 #define NO_ACTION_MACRO
