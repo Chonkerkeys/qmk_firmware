d14bf93f98a88220fe97e32977f222cbd2d37d4a
Chanksy <49041488+Chanksy@users.noreply.github.com>
2022-11-30 11:15:06 +0800

    adjusted google meet shortcuts

--- keyboards/chonkerkeys/core.c
+++ keyboards/chonkerkeys/core.c
@@ -43,7 +43,7 @@ const uint16_t windows_configs[KEYCODE_COUNT][KEY_MACROS_MAX_COUNT] = {
     { KC_LCTRL, KC_E, KC_NO },
     { KC_NO, KC_NO, KC_NO },
     { KC_LCTRL, KC_LALT, KC_H },
-    { KC_LALT, KC_F4, KC_NO },
+    { KC_LCTRL, KC_W, KC_NO },
     // rest
     { KC_AUDIO_VOL_UP, KC_NO, KC_NO },
     { KC_AUDIO_VOL_DOWN, KC_NO, KC_NO },
@@ -86,7 +86,7 @@ const uint16_t macos_configs[KEYCODE_COUNT][KEY_MACROS_MAX_COUNT] = {
     { KC_LGUI, KC_E, KC_NO },
     { KC_NO, KC_NO, KC_NO },
     { KC_LCTRL, KC_LGUI, KC_H },
-    { KC_NO, KC_NO, KC_NO },
+    { KC_LGUI, KC_W, KC_NO },
     // rest
     { KC_AUDIO_VOL_UP, KC_NO, KC_NO },
     { KC_AUDIO_VOL_DOWN, KC_NO, KC_NO },
